b9c44e763a1608ecdfa4b93343067e2657130775
pvin <praaveenranganathan@gmail.com>
2015-05-21 19:30:11 +0530

    added devise capable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520060459) do
+ActiveRecord::Schema.define(version: 20150521130707) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -53,8 +53,6 @@ ActiveRecord::Schema.define(version: 20150520060459) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
